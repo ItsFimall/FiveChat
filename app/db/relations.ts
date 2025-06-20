d0e4d203e0a4a39976f62c87881ac20adfa3ed49
Fimall <fimall@duck.com>
2025-06-20 11:34:17 +0800

    fix: db.query.chat fix

--- app/db/relations.ts
+++ app/db/relations.ts
@@ -1,44 +1,71 @@
-import { llmModels, llmSettingsTable, groupModels, groups, users } from './schema';
-import { relations } from 'drizzle-orm';
+import {
+  llmModels,
+  llmSettingsTable,
+  groupModels,
+  groups,
+  users,
+  chats,
+  messages,
+} from "./schema";
+import { relations } from "drizzle-orm";
+
+export const usersChatsRelations = relations(users, ({ many }) => ({
+  chats: many(chats),
+}));
+
+export const chatsRelations = relations(chats, ({ one, many }) => ({
+  user: one(users, {
+    fields: [chats.userId],
+    references: [users.id],
+  }),
+  messages: many(messages),
+}));
+
+export const messagesRelations = relations(messages, ({ one }) => ({
+  chat: one(chats, {
+    fields: [messages.chatId],
+    references: [chats.id],
+  }),
+}));
 
 export const modelsGroupsRelations = relations(llmModels, ({ many }) => ({
-    groups: many(groupModels),
-}))
+  groups: many(groupModels),
+}));
 
 export const groupsModelsRelations = relations(groups, ({ many }) => ({
-    models: many(groupModels),
-}))
+  models: many(groupModels),
+}));
 
 export const groupsToModelsRelations = relations(groupModels, ({ one }) => ({
-    model: one(llmModels, {
-        fields: [groupModels.modelId],
-        references: [llmModels.id],
-    }),
-    group: one(groups, {
-        fields: [groupModels.groupId],
-        references: [groups.id],
-    }),
-}))
+  model: one(llmModels, {
+    fields: [groupModels.modelId],
+    references: [llmModels.id],
+  }),
+  group: one(groups, {
+    fields: [groupModels.groupId],
+    references: [groups.id],
+  }),
+}));
 
 export const providerModelsRelations = relations(llmSettingsTable, ({ many }) => ({
-    models: many(llmModels),
-}))
+  models: many(llmModels),
+}));
 
 export const modelsProvidersRelations = relations(llmModels, ({ one }) => ({
-    provider: one(llmSettingsTable, {
-        fields: [llmModels.providerId],
-        references: [llmSettingsTable.provider],
-    }),
-}))
+  provider: one(llmSettingsTable, {
+    fields: [llmModels.providerId],
+    references: [llmSettingsTable.provider],
+  }),
+}));
 
 // user and group relations
 export const usersGroupsRelations = relations(groups, ({ many }) => ({
-    users: many(users),
-}))
+  users: many(users),
+}));
 
 export const groupsUsersRelations = relations(users, ({ one }) => ({
-    group: one(groups, {
-        fields: [users.groupId],
-        references: [groups.id],
-    }),
-}))
+  group: one(groups, {
+    fields: [users.groupId],
+    references: [groups.id],
+  }),
+}));
